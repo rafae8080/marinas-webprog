3fc2adf2baa30f234c6d14db93c226e7de8911a0
Rafael Marinas <work.rafael22@gmail.com>
2026-05-25 14:34:58 +0800

    lab-act7

--- marinas-server/index.js
+++ marinas-server/index.js
@@ -40,5 +40,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Server Error" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+module.exports = app;
